a57cde9367fd321297a0f1a6912955abd91dee36
ge-quanyi <1678376818@qq.com>
2023-02-16 20:46:40 +0800

    add cpp

---
+++ python/main.py
@@ -0,0 +1,433 @@
+from pathlib import Path
+
+import openvino.runtime as ov
+from openvino.preprocess import PrePostProcessor
+from openvino.preprocess import ColorFormat
+from openvino.runtime import Layout, Type
+
+import numpy as np
+import cv2
+import time
+import copy
+import torch
+import torchvision
+import math
+
+SCORE_THRESHOLD = 0.2
+NMS_THRESHOLD = 0.4
+CONFIDENCE_THRESHOLD = 0.4
+
+
+def make_divisible(x, divisor):
+    # Returns x evenly divisible by divisor
+    return math.ceil(x / divisor) * divisor
+
+
+def check_img_size(img_size, s=32):
+    # Verify img_size is a multiple of stride s
+    new_size = make_divisible(img_size, int(s))  # ceil gs-multiple
+    if new_size != img_size:
+        print('WARNING: --img-size %g must be multiple of max stride %g, updating to %g' % (img_size, s, new_size))
+    return new_size
+
+
+def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True):
+    # Resize image to a 32-pixel-multiple rectangle https://github.com/ultralytics/yolov3/issues/232
+    shape = img.shape[:2]  # current shape [height, width]
+    if isinstance(new_shape, int):
+        new_shape = (new_shape, new_shape)
+
+    # Scale ratio (new / old)
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+    if not scaleup:  # only scale down, do not scale up (for better test mAP)
+        r = min(r, 1.0)
+
+    # Compute padding
+    ratio = r, r  # width, height ratios
+    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+    if auto:  # minimum rectangle
+        dw, dh = np.mod(dw, 64), np.mod(dh, 64)  # wh padding
+    elif scaleFill:  # stretch
+        dw, dh = 0.0, 0.0
+        new_unpad = (new_shape[1], new_shape[0])
+        ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
+
+    dw /= 2  # divide padding into 2 sides
+    dh /= 2
+
+    if shape[::-1] != new_unpad:  # resize
+        img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
+    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
+    return img, ratio, (dw, dh)
+
+
+def img_process(img_path, long_side=640, stride_max=32):
+    '''
+    图像预处理
+    '''
+    orgimg = cv2.imread(img_path)
+    img0 = copy.deepcopy(orgimg)
+    h0, w0 = orgimg.shape[:2]  # orig hw
+    r = long_side / max(h0, w0)  # resize image to img_size
+    if r != 1:  # always resize down, only resize up if training with augmentation
+        interp = cv2.INTER_AREA if r < 1 else cv2.INTER_LINEAR
+        img0 = cv2.resize(img0, (int(w0 * r), int(h0 * r)), interpolation=interp)
+
+    imgsz = check_img_size(long_side, s=stride_max)  # check img_size
+
+    img = letterbox(img0, new_shape=imgsz, auto=False)[0]  # auto True最小矩形   False固定尺度
+    # Convert
+    """
+    img = img[:, :, ::-1].transpose(2, 0, 1).copy()  # BGR to RGB, to 3x416x416
+    img = torch.from_numpy(img)
+    img = img.float()  # uint8 to fp16/32
+    img /= 255.0  # 0 - 255 to 0.0 - 1.0
+    if img.ndimension() == 3:
+        img = img.unsqueeze(0)
+    """
+    return img, orgimg
+
+
+def xywh2xyxy(x):
+    # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
+    y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
+    y[:, 0] = x[:, 0] - x[:, 2] / 2  # top left x
+    y[:, 1] = x[:, 1] - x[:, 3] / 2  # top left y
+    y[:, 2] = x[:, 0] + x[:, 2] / 2  # bottom right x
+    y[:, 3] = x[:, 1] + x[:, 3] / 2  # bottom right y
+    return y
+
+
+def scale_coords_landmarks(img1_shape, coords, img0_shape, ratio_pad=None):
+    # Rescale coords (xyxy) from img1_shape to img0_shape
+    if ratio_pad is None:  # calculate from img0_shape
+        gain = min(img1_shape[0] / img0_shape[0], img1_shape[1] / img0_shape[1])  # gain  = old / new
+        pad = (img1_shape[1] - img0_shape[1] * gain) / 2, (img1_shape[0] - img0_shape[0] * gain) / 2  # wh padding
+    else:
+        gain = ratio_pad[0][0]
+        pad = ratio_pad[1]
+
+    coords[:, [0, 2, 4, 6]] -= pad[0]  # x padding
+    coords[:, [1, 3, 5, 7]] -= pad[1]  # y padding
+    coords[:, :10] /= gain
+    # clip_coords(coords, img0_shape)
+    coords[:, 0].clamp_(0, img0_shape[1])  # x1
+    coords[:, 1].clamp_(0, img0_shape[0])  # y1
+    coords[:, 2].clamp_(0, img0_shape[1])  # x2
+    coords[:, 3].clamp_(0, img0_shape[0])  # y2
+    coords[:, 4].clamp_(0, img0_shape[1])  # x3
+    coords[:, 5].clamp_(0, img0_shape[0])  # y3
+    coords[:, 6].clamp_(0, img0_shape[1])  # x4
+    coords[:, 7].clamp_(0, img0_shape[0])  # y4
+    # coords[:, 8].clamp_(0, img0_shape[1])  # x5
+    # coords[:, 9].clamp_(0, img0_shape[0])  # y5
+    return coords
+
+
+def clip_coords(boxes, img_shape):
+    # Clip bounding xyxy bounding boxes to image shape (height, width)
+    boxes[:, 0].clamp_(0, img_shape[1])  # x1
+    boxes[:, 1].clamp_(0, img_shape[0])  # y1
+    boxes[:, 2].clamp_(0, img_shape[1])  # x2
+    boxes[:, 3].clamp_(0, img_shape[0])  # y2
+
+
+def box_iou(box1, box2):
+    # https://github.com/pytorch/vision/blob/master/torchvision/ops/boxes.py
+    """
+    Return intersection-over-union (Jaccard index) of boxes.
+    Both sets of boxes are expected to be in (x1, y1, x2, y2) format.
+    Arguments:
+        box1 (Tensor[N, 4])
+        box2 (Tensor[M, 4])
+    Returns:
+        iou (Tensor[N, M]): the NxM matrix containing the pairwise
+            IoU values for every element in boxes1 and boxes2
+    """
+
+    def box_area(box):
+        # box = 4xn
+        return (box[2] - box[0]) * (box[3] - box[1])
+
+    area1 = box_area(box1.T)
+    area2 = box_area(box2.T)
+
+    # inter(N,M) = (rb(N,M,2) - lt(N,M,2)).clamp(0).prod(2)
+    inter = (torch.min(box1[:, None, 2:], box2[:, 2:]) -
+             torch.max(box1[:, None, :2], box2[:, :2])).clamp(0).prod(2)
+    # iou = inter / (area1 + area2 - inter)
+    return inter / (area1[:, None] + area2 - inter)
+
+
+def scale_coords(img1_shape, coords, img0_shape, ratio_pad=None):
+    # Rescale coords (xyxy) from img1_shape to img0_shape
+    if ratio_pad is None:  # calculate from img0_shape
+        gain = min(img1_shape[0] / img0_shape[0], img1_shape[1] / img0_shape[1])  # gain  = old / new
+        pad = (img1_shape[1] - img0_shape[1] * gain) / 2, (img1_shape[0] - img0_shape[0] * gain) / 2  # wh padding
+    else:
+        gain = ratio_pad[0][0]
+        pad = ratio_pad[1]
+
+    coords[:, [0, 2]] -= pad[0]  # x padding
+    coords[:, [1, 3]] -= pad[1]  # y padding
+    coords[:, :4] /= gain
+    clip_coords(coords, img0_shape)
+    return coords
+
+
+def non_max_suppression(
+        prediction,
+        conf_thres=0.6,
+        iou_thres=0.6,
+        classes=None,
+        agnostic=False,
+        multi_label=False,
+        labels=(),
+        max_det=300,
+        nm=0,  # number of masks
+        img=0,
+):
+    """Non-Maximum Suppression (NMS) on inference results to reject overlapping detections
+
+    Returns:
+         list of detections, on (n,6) tensor per image [xyxy, conf, cls]
+    """
+
+    bs = prediction.shape[0]  # batch size
+    nc = prediction.shape[2] - nm - 13  # number of classes
+    xc = prediction[..., 4] > conf_thres  # candidates
+
+    # Checks
+    assert 0 <= conf_thres <= 1, f'Invalid Confidence threshold {conf_thres}, valid values are between 0.0 and 1.0'
+    assert 0 <= iou_thres <= 1, f'Invalid IoU {iou_thres}, valid values are between 0.0 and 1.0'
+
+    # Settings
+    min_wh = 2  # (pixels) minimum box width and height
+    max_wh = 7680  # (pixels) maximum box width and height  7680
+    max_nms = 30000  # maximum number of boxes into torchvision.ops.nms()
+    time_limit = 0.5 + 0.05 * bs  # seconds to quit after
+    redundant = True  # require redundant detections
+    multi_label = nc > 1  # multiple labels per box (adds 0.5ms/img)
+    merge = False  # use merge-NMS
+
+    t = time.time()
+
+    mi = 5 + nc  # mask start index
+
+    output = [torch.zeros((0, 49))] * bs  # 49
+    for xi, x in enumerate(prediction):  # image index, image inference
+        # Apply constraints
+        # x[((x[..., 2:4] < min_wh) | (x[..., 2:4] > max_wh)).any(1), 4] = 0  # width-height
+
+        x = x[xc[xi]]  # confidence
+        # print(x[0])
+        # Cat apriori labels if autolabelling
+
+        if labels and len(labels[xi]):
+            lb = labels[xi]
+            v = torch.zeros((len(lb), nc + nm + 5), device=x.device)
+            v[:, :4] = lb[:, 1:5]  # box
+            v[:, 4] = 1.0  # conf
+            v[range(len(lb)), lb[:, 0].long() + 5] = 1.0  # cls
+            x = torch.cat((x, v), 0)
+
+        # If none remain process next image
+        if not x.shape[0]:
+            continue
+
+        # Compute conf
+        x[:, 13:] *= x[:, 4:5]  # conf = obj_conf * cls_conf
+        # x,y,w,h,conf, x1,y1,x2,y2,x3,y3,x4,y4, c1,c2,c3, ... , c36
+        # Box/Mask
+
+        box = xywh2xyxy(x[:, :4])  # center_x, center_y, width, height) to (x1, y1, x2, y2)
+
+        mask = x[:, mi:]  # zero columns if no masks
+        # points_my = x[:,5:13]
+
+        # points_my = scale_coords_landmarks((640, 640), x[:, 5:13], img.shape)
+
+        # Detections matrix nx6 (xyxy, conf, cls)
+
+        if multi_label:
+            i, j = (x[:, 13:] > conf_thres).nonzero(as_tuple=False).T
+            x = torch.cat((box[i], x[i, j + 13, None], x[i, 5:13], j[:, None].float()), 1)
+        else:  # best class only
+            conf, j = x[:, 13:].max(1, keepdim=True)
+            x = torch.cat((box, conf, x[:, 5:13], j.float()), 1)[conf.view(-1) > conf_thres]
+
+        # mask2 = x[:, mi:]
+        # x = torch.cat((x[:, :4], x[:, 4:12], x[:, 12:13], x[:, 13:14].float(), mask2), 1)[x[:, 12:13].view(-1) < 1]
+
+        # print("x2:",x)
+        # Filter by class
+        if classes is not None:
+            x = x[(x[:, 5:6] == torch.tensor(classes, device=x.device)).any(1)]
+
+        # Apply finite constraint
+        # if not torch.isfinite(x).all():
+        #     x = x[torch.isfinite(x).all(1)]
+
+        # Check shape
+
+        n = x.shape[0]  # number of boxes
+        if not n:  # no boxes
+            continue
+        elif n > max_nms:  # excess boxes
+            x = x[x[:, 13].argsort(descending=True)[:max_nms]]  # sort by confidence
+        else:
+            x = x[x[:, 13].argsort(descending=True)]  # sort by confidence
+        # Batched NMS
+        c = x[:, 13:14] * (0 if agnostic else max_wh)  # classes
+        boxes, scores = x[:, :4] + c, x[:, 4]  # boxes (offset by class), scores
+        i = torchvision.ops.nms(boxes, scores, iou_thres)  # NMS
+        if i.shape[0] > max_det:  # limit detections
+            i = i[:max_det]
+        if merge and (1 < n < 3E3):  # Merge NMS (boxes merged using weighted mean)
+            # update boxes as boxes(i,4) = weights(i,n) * boxes(n,4)
+            iou = box_iou(boxes[i], boxes) > iou_thres  # iou matrix
+            weights = iou * scores[None]  # box weights
+            x[i, :4] = torch.mm(weights, x[:, :4]).float() / weights.sum(1, keepdim=True)  # merged boxes
+            if redundant:
+                i = i[iou.sum(1) > 1]  # require redundancy
+        output[xi] = x[i]
+        '''
+        if mps:
+            output[xi] = output[xi].to(device)
+        '''
+        if (time.time() - t) > time_limit:
+            # LOGGER.warning(f'WARNING ⚠️ NMS time limit {time_limit:.3f}s exceeded')
+            break  # time limit exceeded
+    return output
+
+
+def resize_and_pad(image, new_shape):
+    old_size = image.shape[:2]
+    ratio = float(new_shape[-1] / max(old_size))  # fix to accept also rectangular images
+    new_size = tuple([int(x * ratio) for x in old_size])
+
+    image = cv2.resize(image, (new_size[1], new_size[0]))
+
+    delta_w = new_shape[1] - new_size[1]
+    delta_h = new_shape[0] - new_size[0]
+
+    color = [100, 100, 100]
+    new_im = cv2.copyMakeBorder(image, 0, delta_h, 0, delta_w, cv2.BORDER_CONSTANT, value=color)
+
+    return new_im
+
+
+def show_results(img, xyxy, conf, landmarks, class_num):
+    h, w, c = img.shape
+    tl = 1 or round(0.002 * (h + w) / 2) + 1  # line/font thickness
+    x1 = int(xyxy[0])
+    y1 = int(xyxy[1])
+    x2 = int(xyxy[2])
+    y2 = int(xyxy[3])
+    img = img.copy()
+
+    cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), thickness=tl, lineType=cv2.LINE_AA)
+
+    clors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0)]
+
+    point = []
+    for i in range(4):
+        point.append(int(landmarks[2 * i]))
+        point.append(int(landmarks[2 * i + 1]))
+        point_x = int(landmarks[2 * i])
+        point_y = int(landmarks[2 * i + 1])
+        cv2.circle(img, (point_x, point_y), tl + 1, clors[i], -1)
+    cv2.line(img, (point[0], point[1]), (point[4], point[5]), (0, 0, 255), 5)
+    cv2.line(img, (point[2], point[3]), (point[6], point[7]), (0, 0, 255), 5)
+    cv2.line(img, (point[0], point[1]), (point[2], point[3]), (0, 0, 255), 5)
+    cv2.line(img, (point[4], point[5]), (point[6], point[7]), (0, 0, 255), 5)
+    # print(point)
+
+    tf = max(tl - 1, 1)  # font thickness
+    label = str(conf)[:5]
+    cv2.putText(img, "class:" + str(class_num), (x1 + 50, y1 - 2), 0, tl / 3, [225, 255, 255], thickness=tf,
+                lineType=cv2.LINE_AA)
+    cv2.putText(img, label, (x1, y1 - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+    return img
+
+
+def main():
+    t0 = time.time()
+    img, im0 = img_process("aaa.jpg")
+    # Step 1. Initialize OpenVINO Runtime core
+    t1 = time.time()
+    core = ov.Core()
+    # Step 2. Read a model
+    model = core.read_model(str(Path("./model/rm-net16.xml")))
+
+    # Step 3. Read input image
+
+    # resize image
+    # img_reized = resize_and_pad(img, (640, 640))
+
+    # Step 4. Inizialize Preprocessing for the model
+    ppp = PrePostProcessor(model)
+    # Specify input image format
+    ppp.input().tensor().set_element_type(Type.u8).set_layout(Layout("NHWC")).set_color_format(ColorFormat.BGR)
+    #  Specify preprocess pipeline to input image without resizing
+    ppp.input().preprocess().convert_element_type(Type.f32).convert_color(ColorFormat.RGB).scale([255., 255., 255.])
+    # Specify model's input layout
+    ppp.input().model().set_layout(Layout("NCHW"))
+    #  Specify output results format
+    ppp.output().tensor().set_element_type(Type.f32)
+    # Embed above steps in the graph
+    model = ppp.build()
+    compiled_model = core.compile_model(model, "CPU")
+
+    # Step 5. Create tensor from image
+    input_tensor = np.expand_dims(img, 0)
+
+    # Step 6. Create an infer request for model inference
+    infer_request = compiled_model.create_infer_request()
+    infer_request.infer({0: input_tensor})
+
+    # Step 7. Retrieve inference results
+    output = infer_request.get_output_tensor()
+    t2 = time.time()
+    print(output.data.shape)
+
+    pred = non_max_suppression(torch.from_numpy(output.data), conf_thres=0.5, iou_thres=0.5, img=img)
+
+    print(len(pred[0]), 'face' if len(pred[0]) == 1 else 'faces')
+    print(pred[0][0])
+    print("img shape {}".format(img.shape))
+    print("img0 shape {}".format(im0.shape))
+    for i, det in enumerate(pred):
+        if len(det):
+            # Rescale boxes from img_size to im0 size
+            det[:, :4] = scale_coords(img.shape, det[:, :4], im0.shape).round()
+
+            # Print results
+            for c in det[:, -1].unique():
+                n = (det[:, -1] == c).sum()  # detections per class
+
+            det[:, 5:13] = scale_coords_landmarks(img.shape, det[:, 5:13], im0.shape).round()
+
+            for j in range(det.size()[0]):
+                xyxy = det[j, :4].view(-1).tolist()
+                conf = det[j, 4].cpu().numpy()
+                landmarks = det[j, 5:13].view(-1).tolist()
+                class_num = det[j, 13].cpu().numpy()
+                print("ssssss")
+                print(im0.shape)
+                im0 = show_results(im0, xyxy, conf, landmarks, class_num)
+
+            # cv2.imshow('result', im0)
+            # k = cv2.waitKey(0)
+
+    t3 = time.time()
+    print("preprocess cost {}".format(t1 - t0))
+    print("inference cost {} ".format(t2 - t1))
+    print("postprocess cost {}".format(t3 - t2))
+
+
+if __name__ == '__main__':
+    main()
